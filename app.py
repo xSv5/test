22243240a83cb6df6d0b1bee6aa0b824a936c6b9
xSv5 <152936317+xSv5@users.noreply.github.com>
2025-04-25 17:39:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,9 @@ def new():
     return jsonify({'status': 'added'}), 200
 
 if __name__ == '__main__':
-    app.run()
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
