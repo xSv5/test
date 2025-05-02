0dfddb172fc649a753217bed49e3dcb2460d520c
xSv5 <152936317+xSv5@users.noreply.github.com>
2025-05-01 23:05:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,29 @@ from flask import Flask, request, Response
 import requests
 import os
 
-
 app = Flask(__name__)
-TARGET_URL = 'https://xsv4mailsender.pythonanywhere.com/verify'
+TARGET_URL = 'https://xsv4mailsender.pythonanywhere.com'
 
-@app.route('/verify', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
-def proxy():
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+def proxy(path):
+    url = f"{TARGET_URL}/{path}"
+    
+    # Forward the request to the target URL
     resp = requests.request(
         method=request.method,
-        url=TARGET_URL,
+        url=url,
         headers={k: v for k, v in request.headers if k.lower() != 'host'},
         data=request.get_data(),
         cookies=request.cookies,
-        allow_redirects=False
+        allow_redirects=False,
+        params=request.args
     )
+    
+    # Exclude these headers from the response
     excluded = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = [(k, v) for k, v in resp.raw.headers.items() if k.lower() not in excluded]
+    
     return Response(resp.content, resp.status_code, headers)
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
